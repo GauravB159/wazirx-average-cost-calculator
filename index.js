69ae30c70f82312b7f82cdbb5a15d4c7d92df343
Gaurav Bhagchandani <gb@pykih.com>
2021-08-13 18:14:46 +0530

    Added support for USDT and FIFO average cost method

--- index.js
+++ index.js
@@ -28,6 +28,12 @@
         a.click();
     }
 
+    function convertCurrency(object, tickers, currency){
+        if(currency !== "USDT") return object;
+        let value = parseFloat(tickers["usdtinr"].last);
+        return object * value;
+    }
+
     let orders = document.querySelector(".completed-orders");
     if(!orders){
         alert("No trades found. Make sure you are on WazirX, in the Exchange tab.");
@@ -47,56 +53,60 @@
             return;
         }
         let elements = [];
+        let buyCurrency = [];
         elementsContainer.forEach((container)=>{
             let temp = container.querySelectorAll(":scope>div:first-child>div>span:first-child");
             elements.push(...Array.from(temp));
+            
         });
-        console.log(elements, "ELS2");
-
+        elements.forEach((element)=>{
+            if(element.innerText.split("\n")[1] === "INR" || element.innerText.split("\n")[1] === "USDT"){
+                buyCurrency.push(element.innerText.split("\n")[1])
+            }
+        });
         elements = elements.map((a)=> a.innerText.split("\n")[0]);
         let elementsChunked = chunkArray(elements, 4);
         let header = ["Pair", "Amount", "Price", "Total"];
-        elementsObjects = elementsChunked.map((a)=>{
+        elementsObjects = elementsChunked.map((a, i)=>{
             let object = {};
             a.forEach((b, i) => {
                 if(i) object[header[i]] = parseFloat(b.replaceAll(",", ""));
                 else object[header[i]] = b;
             });
+            object["Currency"] = buyCurrency[i];
             return object;
         });
         let sellElementsContainer = Array.from(allElements).filter(div => color !== window.getComputedStyle(div, ":before").backgroundColor);
         let sellElements = [];
+        let sellCurrency = [];
+
         sellElementsContainer.forEach((container)=>{
             let temp = container.querySelectorAll(":scope>div:first-child>div>span:first-child");
             sellElements.push(...Array.from(temp));
         });
-        console.log(sellElements, "ELS");
+
+        sellElements.forEach((element)=>{
+            if(element.innerText.split("\n")[1] === "INR" || element.innerText.split("\n")[1] === "USDT"){
+                sellCurrency.push(element.innerText.split("\n")[1])
+            }
+        });
+
         sellElements = Array.from(sellElements).map((a)=> a.innerText.split("\n")[0]);
         let sellElementsChunked = chunkArray(sellElements, 4);
-        sellElementsObjects = sellElementsChunked.map((a)=>{
+        sellElementsObjects = sellElementsChunked.map((a, i)=>{
             let object = {};
             a.forEach((b, i) => {
                 if(i) object[header[i]] = parseFloat(b.replaceAll(",", ""));
                 else object[header[i]] = b;
             });
+            object["Currency"] = sellCurrency[i];
             return object;
         });
+        console.log(sellElementsObjects, elementsObjects);
         fetch('https://api.wazirx.com/api/v2/tickers').then(response => response.json()).then(tickers =>{
-            let summationObject = {};
-            elementsObjects.forEach((obj)=>{
-                if(!summationObject.hasOwnProperty(obj.Pair)){
-                    summationObject[obj.Pair] = {
-                        "Amount": 0,
-                        "Total": 0,
-                        "Current": parseFloat(tickers[obj.Pair.toLowerCase() + "inr"].last)
-                    }
-                }
-                summationObject[obj.Pair]["Amount"] += obj.Amount;
-                summationObject[obj.Pair]["Total"] += obj.Total;  
-            });
-
             let sellSummationObject = {};
             sellElementsObjects.forEach((obj)=>{
+                if(obj.Pair === "USDT") return;
                 if(!sellSummationObject.hasOwnProperty(obj.Pair)){
                     sellSummationObject[obj.Pair] = {
                         "Amount": 0,
@@ -104,22 +114,49 @@
                     }
                 }
                 sellSummationObject[obj.Pair]["Amount"] += obj.Amount;
-                sellSummationObject[obj.Pair]["Total"] += obj.Total;
+                sellSummationObject[obj.Pair]["Total"] += convertCurrency(obj.Total, tickers, obj.Currency);
             });
+            
+            let summationObject = {};
+            elementsObjects.reverse().forEach((obj)=>{
+                if(obj.Pair === "USDT") return;
+                if(!summationObject.hasOwnProperty(obj.Pair)){
+                    summationObject[obj.Pair] = {
+                        "Amount": 0,
+                        "Total": 0,
+                        "Current": convertCurrency(parseFloat(tickers[obj.Pair.toLowerCase() + obj.Currency.toLowerCase()] && tickers[obj.Pair.toLowerCase() + obj.Currency.toLowerCase()].last), tickers, obj.Currency)
+                    }
+                }
+                if(sellSummationObject.hasOwnProperty(obj.Pair)) {
+                    let sellAmount = sellSummationObject[obj.Pair]["Amount"];
+                    console.log(sellAmount,obj.Amount, obj.Pair, sellSummationObject, "SELL");
+                    if(sellAmount > obj.Amount){
+                        sellAmount = sellAmount - obj.Amount;
+                        obj.Amount = 0;
+                    }else{
+                        obj.Amount = obj.Amount - sellAmount;
+                        sellAmount = 0;
+                    }
+                    console.log(obj.Amount, "AMOUNT");
+                    sellSummationObject[obj.Pair]["Amount"] = sellAmount;
+                }
+                summationObject[obj.Pair]["Amount"] += obj.Amount;
+                summationObject[obj.Pair]["Total"] += convertCurrency(obj.Price * obj.Amount, tickers, obj.Currency);  
+            });
+
+         
             let totalStats = {
                 "Total Profit/Loss": 0
             };
             let summationArray = [];
             let header = ["Amount", "Total", "Current", "Price", "Difference", "Profit/Loss"];
             Object.keys(summationObject).forEach((key, i)=>{
-                summationObject[key]["Price"] = summationObject[key]["Total"] / summationObject[key]["Amount"];
-                if(sellSummationObject.hasOwnProperty(key)) {
-                    summationObject[key]["Amount"] = summationObject[key]["Amount"] - sellSummationObject[key]["Amount"];
-                    summationObject[key]["Total"] = summationObject[key]["Amount"] * summationObject[key]["Price"];
-                }
+                if(summationObject[key]["Amount"] === 0) summationObject[key]["Price"] = 0;
+                else summationObject[key]["Price"] = summationObject[key]["Total"] / summationObject[key]["Amount"];
                 summationObject[key]["Difference"] = summationObject[key]["Current"] - summationObject[key]["Price"];
                 summationObject[key]["Profit/Loss"] = summationObject[key]["Amount"] * summationObject[key]["Difference"];
-                summationObject[key]["Percentage Profit/Loss"] = (summationObject[key]["Profit/Loss"] / summationObject[key]["Total"]) * 100;
+                if(summationObject[key]["Profit/Loss"] === 0) summationObject[key]["Percentage Profit/Loss"] = 0;
+                else summationObject[key]["Percentage Profit/Loss"] = (summationObject[key]["Profit/Loss"] / summationObject[key]["Total"]) * 100;
                 totalStats["Total Profit/Loss"] += summationObject[key]["Profit/Loss"];
                 summationArray.push([]); 
                 summationArray[i].push(key); 
